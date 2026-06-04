a7993bf1947ec95c120fe4100265de342839abac
Zack Feltovich <feltoza@smbg.vic.edu.au>
2026-06-04 14:28:17 +1000

    updated kernel
    improved loading of modules (do not run as a windows app; it is a kernel)

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-@echo off
+echo on
 setlocal enabledelayedexpansion
 
 if not exist build mkdir build
@@ -15,7 +15,7 @@ echo Linking kernel image...
 g++ -m32 -ffreestanding -nostdlib -Wl,-Ttext=0x100000 -Wl,--image-base=0x100000 -o kernel.exe kernel.o afs_set.o disk.o screen.o
 C:\Users\feltoza\gcc\bin\..\lib\gcc\x86_64-w64-mingw32\15.2.0\..\..\..\..\x86_64-w64-mingw32\bin\objcopy.exe -O binary kernel.exe kernel.bin
 
-move /Y kernel.o afs_set.o disk.o kernel.elf kernel.bin ..\build\
+move /Y kernel.o afs_set.o disk.o screen.o kernel.exe kernel.bin ..\build
 cd ..
 
 echo Assembling boot components...
---
+++ kernel/temp_move.cmd
@@ -0,0 +1,1 @@
+move /Y kernel.o afs_set.o disk.o kernel.elf kernel.bin ..\build\
